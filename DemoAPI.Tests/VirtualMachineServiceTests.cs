010ca344f678d62e93e9f196747fa18d8adee2c5
Vithushan Mahendran <vithushan.mahendran@hotmail.com>
2025-06-10 00:16:19 +0200

    add test project but not finished yet

---
+++ DemoAPI.Tests/VirtualMachineServiceTests.cs
@@ -0,0 +1,16 @@
+﻿using DemoAPI.Services;
+public class VirtualMachineServiceTests
+{
+    private readonly VirtualMachineService _vmService;
+    public VirtualMachineServiceTests(VirtualMachineService vmService)
+    {
+        _vmService = vmService;
+    }
+
+    [Fact]
+    public void GetVM_NotNullAndNotEmpty()
+    {
+        var svm = _vmService.GetVirtualMachineAsync("");
+        Assert.NotNull(svm);
+    }
+}
